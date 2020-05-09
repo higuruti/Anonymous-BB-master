b14e9425e90afffd99d00e060e003cd4e9c4ef49
higuruti <fumiya2324@gmail.com>
2020-05-09 18:46:52 +0900

    add detail comment

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+// 必要なモジュールを取り込んでいる
+
+
 var express = require('express');
+// expressのインスタンス化
 var app = express();
+// postの中身を取得するためのモジュール
 var bodyParser = require('body-parser');
+// ejsファイルという、htmlファイルを動的に変化させるためのモジュール
 var ejs = require('ejs');
+// mysqlを扱うためのモジュール
 var mysql = require('mysql');
+// httpサーバーを作っている
 var server = require('http').createServer(app);
+// websocket通信をするためのモジュールを取り込みインスタンス化
 var io = require('socket.io').listen(server);
+// セッション管理をするためのモジュール
 var session = require('express-session');
+// SQL文を解析するためのモジュールとその設定をしている
 var knex = require('knex')({
   dialect: 'mysql',
   connection: {
@@ -16,32 +27,42 @@ var knex = require('knex')({
     charset: 'utf8'
   }
 });
+// 上記で設定したデータベースのテーブルをモデル化とか書いてあるけどよくわからん。
+// Bookshelfを使うためには必要。
 var Bookshelf = require('bookshelf')(knex);
 var Users = Bookshelf.Model.extend({
   tableName: 'users'
 });
 
+// セッション管理をするための設定
 var session_opt = {
   secret: 'keyboard cat',
   resave: false,
   saveUninitialized: false,
   cookie: {maxAge:60*60*1000}
 };
+// セッション管理するためには必要な文。よくわからん
 app.use(session(session_opt));
 
+// ejsファイルを使うためのエンジンを設定。
 app.engine('ejs', ejs.renderFile);
+// postの内容を取得するために必要。とにかく必要なんだ
 app.use(bodyParser.urlencoded({extended: false}));
 
 
 // ---------------メッセージ--------------------
+// urlが/で、メソッドがgetの時の動き
 app.get('/', function(request, response){
+  // ログインしていなかった場合ログイン画面に飛ばす
   if(request.session.login == null){
     response.redirect('/login');
   }else{
+    // ログインしていた場合chatの画面に行く
     response.render('index.ejs',{});  
   }
 });
 
+//websocket での通信の処理
 io.on('connection', function(socket){
   socket.on('chat', function(msg){
     io.emit('chat', msg);
@@ -49,25 +70,33 @@ io.on('connection', function(socket){
 });
 
 // -----------------ユーザーのログイン処理--------------------
+// ログイン画面
 app.get('/login', function(request, response){
   response.render('login.ejs', {});
 });
 
+// 名前とパスワードが送られてきたときの処理
 app.post('/login', function(request, response){
+  // userName、password変数に送られてきたユーザー名、名前を入れる
   var userName = request.body.name;
   var password = request.body.password;
 
+  // データベースに接続して送られてきたアカウントが存在するか調べる
   Users.query({where: {name: userName}, andWhere: {password: password}}).fetch().then((model)=>{
     if(model == null){
       // 間違ったログイン名とパスワードを入力するとエラー発生　原因不明
       response.render('login.ejs', {});
     }else{
+      // 存在した場合、セッション管理をしている変数にユーザーのデータを入れてる。
+      // ここら辺もう少し調べとく
       request.session.login = model.attributes;
+      // /でリダイレクト　チャット画面に移行
       response.redirect('/');
     }
   })
 
 });
 
+// ポート番号3000で待ち状態
 server.listen(3000);
 
