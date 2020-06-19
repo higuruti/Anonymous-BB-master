6aec92fce53443423eec8756cf65345bc0efd312
yassi-github <s19t312@stu.kagawa-u.ac.jp>
2020-06-20 06:22:49 +0900

    トピック一つのとき選択できないのを修正

--- public/topic.js
+++ public/topic.js
@@ -1,5 +1,11 @@
 let radioButtonElements = document.forms.chooseTopics.elements;
 for (let i = 0; i < radioButtonElements.length - 1; i++) {
+    if (!i) {
+        radioButtonElements.topic.addEventListener("click", () => {
+            radioButtonElements.submitBtn.disabled = 0;
+        });
+        break;
+    }
     radioButtonElements.topic[i].addEventListener("click", () => {
         radioButtonElements.submitBtn.disabled = 0;
     });
